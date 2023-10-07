36fa61adb598d9f8a2be8fc1e7ce711b9a65a770
Irina Klepova <ivklepova23@gmail.com>
2023-10-07 18:42:41 +0400

    добавлены сеттер и геттер, staticmethod, classmethod

--- src/item.py
+++ src/item.py
@@ -1,3 +1,9 @@
+import csv
+
+
+file_name = 'src/items.csv'
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +19,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name  # атрибут `name` сделать приватным
         self.price = price
         self.quantity = quantity
 
@@ -33,3 +39,37 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
+
+    """добавить геттер и сеттер для `name`, используя @property"""
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        """В сеттере `name` проверять, что длина наименования товара не больше 10 символов.
+            В противном случае, обрезать строку (оставить первые 10 символов)"""
+        if len(new_name) < 10:
+            self.__name = new_name
+        else:
+            self.__name = new_name[:10]
+
+    @classmethod
+    def instantiate_from_csv(cls, file_name):
+        """Класс-метод, инициализирующий экземпляры класса `Item` данными из файла _src/items.csv_"""
+        cls.all.clear()
+        with open(file_name, encoding='windows-1251') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=',')
+            items = list(reader)
+            for item in items:
+                ob_item = cls(item['name'], Item.string_to_number(item['price']),
+                              Item.string_to_number(item['quantity']))
+
+    @staticmethod
+    def string_to_number(line):
+        """Статический метод, возвращающий число из числа-строки"""
+        num = ''
+        for symbol in line:
+            if symbol.isdigit() or symbol == ".":
+                num += symbol
+        return int(float(num))
