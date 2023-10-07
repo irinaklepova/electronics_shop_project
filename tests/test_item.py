2fff10e3f73c573f09060219c74371f2ae33ac39
Irina Klepova <ivklepova23@gmail.com>
2023-10-07 18:43:39 +0400

    добавлены тесты для методов

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,6 +1,12 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 import pytest
+import csv
 from src.item import Item
+
+
+item1 = Item("Смартфон", 10000, 20)
+item2 = Item("Ноутбук", 20000, 5)
+file_name = 'src/items.csv'
 
 
 @pytest.fixture
@@ -19,3 +25,22 @@ def test_calculate_total_price(item_ob):
 def test_apply_discount(item_ob):
     item_ob.apply_discount()
     assert item_ob.price == 200 * item_ob.pay_rate
+
+
+
+def test_name_setter():
+    item1.name = 'СуперСмартфон'
+    assert item1.name == 'СуперСмарт'
+    item1.name = 'Смартфон'
+    assert item1.name == 'Смартфон'
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv(file_name)
+    assert len(Item.all) == 5
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
