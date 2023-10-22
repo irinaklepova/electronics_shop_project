9f23ad0b7da2fe48329d36b26c6f8998360268f6
Irina Klepova <ivklepova23@gmail.com>
2023-10-22 20:43:49 +0400

    добавлен сеттер number_of_sim

--- src/phone.py
+++ src/phone.py
@@ -13,9 +13,9 @@ class Phone(Item):
     def number_of_sim(self):
         return self.__number_of_sim
 
-    # @number_of_sim.setter
-    # def number_of_sim(self, number: int):
-    #     if isinstance(number, int) and number > 0:
-    #         self.__number_of_sim = number
-    #     else:
-    #         raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля")
+    @number_of_sim.setter
+    def number_of_sim(self, number: int):
+        if isinstance(number, int) and number > 0:
+            self.__number_of_sim = number
+        else:
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля")
